b85180b49046483e1b68e94b5cad55156b429845
Aryan <pakhrinp242@gmail.com>
2025-02-18 20:57:49 +0545

    Recipe

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+// import RecipePage from "./components/RecipePage"
+import CardComponent from "./landing_page/card/card_component"
+function App() {
+
+  return (
+ <>
+ <CardComponent />
+ </>
+  )
+}
+
+export default App
---
+++ src/landing_page/card/index.tsx
@@ -0,0 +1,8 @@
+import CardComponent from "./card_component";
+function Card() {
+    return (
+        <CardComponent title="title" description="description" image="image" />
+    )
+}
+
+export default Card;
