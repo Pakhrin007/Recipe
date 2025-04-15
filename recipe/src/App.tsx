de52153cb41e8425fd8c4fa5192244f188c65419
Aryan <pakhrinp242@gmail.com>
2025-04-15 08:51:10 +0545

    logout modal added

--- recipe/src/App.tsx
+++ recipe/src/App.tsx
@@ -3,6 +3,7 @@ import LandingPage from "./Component/Landing-Page/index";
 import { useSelector } from "react-redux";
 import UserSidebarWrapper from "./Component/User/SideBar/index";
 import ChefSidebarWrapper from "./Component/Chef/SideBar/index";
+// import Modal from "./UI/Modal";
 const App = () => {
   const role=useSelector((state:any)=>state.role.role);
   return (
@@ -33,8 +34,9 @@ const App = () => {
       );
   }
 })()}
+  {/* <Modal/> */}
       
-    </div>
+    </div>  
    
   );
 };
